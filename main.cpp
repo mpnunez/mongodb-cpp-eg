ad2a2fb8bb461675dbf95c9df46959e89e46f454
Marcel Nunez <marcelpnunez@gmail.com>
2024-09-10 18:55:38 -0700

    Read uri from file

--- main.cpp
+++ main.cpp
@@ -1,13 +1,31 @@
 #include <bsoncxx/json.hpp>
 #include <mongocxx/client.hpp>
 #include <mongocxx/instance.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+std::string get_uri(std::string fname){
+    std::string item_name;
+    std::ifstream nameFileout;
+
+    nameFileout.open(fname);
+    std::getline(nameFileout, item_name);
+    nameFileout.close();
+
+    return item_name;
+}
+
 int main()
 {
   try
   {
+    auto uri_string = get_uri("uri.txt");
+
     // Create an instance.
     mongocxx::instance inst{};
-    const auto uri = mongocxx::uri{"/your/uri/here"};
+    const auto uri = mongocxx::uri{uri_string};
     // Set the version of the Stable API on the client
     mongocxx::options::client client_options;
     const auto api = mongocxx::options::server_api{mongocxx::options::server_api::version::k_version_1};
